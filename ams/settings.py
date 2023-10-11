46fd5331279ae8a2c70a5121dd1a1b9ca56c638c
Abrantepah <christianidanfrowne@gmail.com>
2023-10-11 11:42:06 +0000

    postgresql

--- ams/settings.py
+++ ams/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'ams.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',   # Replace with your database name
+        'USER': 'postgres',    # Replace with your username
+        'PASSWORD': 'AATi6H4FxFy7Ti1lVmj4',  # Replace with your password
+        'HOST': 'containers-us-west-42.railway.app',   # Leave empty for localhost
+        'PORT': '7117',   # Leave empty for the default port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
