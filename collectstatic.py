126fcb4aab03653848d48b209e6cd7d84367f7a3
Abrantepah <christianidanfrowne@gmail.com>
2023-10-11 02:38:45 +0000

    progress

--- collectstatic.py
+++
@@ -1,8 +0,0 @@
-# collectstatic.py
-
-import os
-from django.core.management import call_command
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ams.settings")
-    call_command("collectstatic", interactive=False)
